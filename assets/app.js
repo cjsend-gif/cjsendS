3cea97538685057e3abd6a6d3e2a0372786fc052
cjsend-gif <erickparkcha@gmail.com>
2025-09-22 22:52:02 +0900

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -4,7 +4,7 @@
  *   예) https://cjsend.erickparkcha.workers.dev
  * =========================== */
 
-const API = 'https://cjsend2.erickparkcha.workers.dev/'; // ← 워커 주소로 교체
+const API = 'https://cisend2.erickparkcha.workers.dev'; // ← 네 워커 주소 정확히!
 
 /* ---------- 공통 유틸 ---------- */
 async function apiGet(path) {
